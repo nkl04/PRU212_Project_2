838259d6e33f5142ea135c67cfb8824b19c37bf9
nkl04 <longnguyenkim2707@gmail.com>
2025-02-19 10:35:07 +0700

    feat: splash scene

---
+++ Assets/Scripts/UI/LoadingBar.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingBar : MonoBehaviour
+{
+    [SerializeField] private Image fill;
+    [SerializeField] private TextMeshProUGUI loadingNumberText;
+
+    private void Start()
+    {
+        fill.fillAmount = 0;
+        StartCoroutine(LoadingBarCoroutine());
+    }
+
+    private IEnumerator LoadingBarCoroutine()
+    {
+        // incremental loading
+        while (fill.fillAmount < 1)
+        {
+            fill.fillAmount += 0.01f;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private void Update()
+    {
+        loadingNumberText.text = (fill.fillAmount * 100).ToString("F0") + "%";
+    }
+}
